6c99fc7129ab2a2fc88f658e203ff465a6ece918
sunyu <172569558@qq.com>
2020-04-30 13:49:56 +0800

    取掉河西双公示中的2019字样

--- src/ResourceCatalog/Model/IResourceCatalogTypeName.php
+++ src/ResourceCatalog/Model/IResourceCatalogTypeName.php
@@ -5,8 +5,8 @@ interface IResourceCatalogTypeName
 {
     const TYPE_NAME = array(
         IResourceCatalog::TYPE['NULL'] => '',
-        IResourceCatalog::TYPE['XBXZCF'] => '2019行政处罚公示信息',
-        IResourceCatalog::TYPE['XBXZXK'] => '2019行政许可公示信息',
+        IResourceCatalog::TYPE['XBXZCF'] => '行政处罚公示信息',
+        IResourceCatalog::TYPE['XBXZXK'] => '行政许可公示信息',
         IResourceCatalog::TYPE['TEST'] => '演示资源目录',
         IResourceCatalog::TYPE['JGJCDXZJL'] => '价格监测的行政奖励',
         IResourceCatalog::TYPE['YXQNZYZ'] => '优秀青年志愿者',
